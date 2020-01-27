cb4690087e535b573b2817b1646d0cce5f4a1ba6
Apple <apple@Mac-7831c1bea138.local>
2020-01-27 14:45:31 -0800

    created game logic

--- memory--game/js/main.js
+++ memory--game/js/main.js
@@ -1,38 +1,24 @@
 const cards = ["Queen", "Queen", "King", "King"];
 let cardsInPlay = [];
-let cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped" + " " + cardOne);
-
-let cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped" + " " + cardTwo);
 
 
-if (cardsInPlay.length === 2) {
-	console.log("True.");
-} if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert ("You found a match!");
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  	alert("You found a match!");
 } else {
-	alert ("Try again :(");
+  	alert("Sorry, try again.");}
 }
-/*
-if (cardsInPlay[0] === cardsInPlay[1]) {
-	console.log("You found a match!");
-} else(cardsInPlay[0] = cardsInPlay[1]) {
-	console.log("Try Again :(");
+
+
+ 
+function flipCard(cardId){
+	console.log("User flipped" + " " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+ if (cardsInPlay.length === 2) {
+	checkForMatch(); 
+	}
 }
-	
 
-
-	/*} else if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You've found a match!");
-	} else alert("Sorry, try again.");
-};*/
-
-// I think iI have the symtax wrong.
-
-
-/*alert("Hello, Dupas!");*/
-
-
+flipCard(0);
+flipCard(2);
