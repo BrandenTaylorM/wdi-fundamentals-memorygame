179c053cfba1f549a1df156b8cd7702f5a1123ec
Apple <apple@Mac-7831c1bea138.local>
2020-01-27 16:01:29 -0800

    added object and logic

--- memory--game/js/main.js
+++ memory--game/js/main.js
@@ -1,4 +1,26 @@
-const cards = ["Queen", "Queen", "King", "King"];
+const cards = [
+{
+	rank : "queen",
+	suit : "hearts",
+	cardImage : "images/queen-of-hearts.png"
+},
+{
+	rank : "queen",
+	suit : "diamonds",
+	cardImage : "images/queen-of-diamonds.png"
+},
+{
+	rank : "king",
+	suit : "hearts",
+	cardImage : "images/king-of-hearts.png"
+},
+{
+	rank : "king",
+	suit : "diamonds",
+	cardImage : "images/king-of-diamonds.png"
+}
+];
+
 let cardsInPlay = [];
 
 
@@ -12,13 +34,23 @@ function checkForMatch(){
 
  
 function flipCard(cardId){
-	console.log("User flipped" + " " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped" + " " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+
 
  if (cardsInPlay.length === 2) {
 	checkForMatch(); 
 	}
 }
 
+/*
+console.log(cardImage)
+console.log(suit)
+*/
+
+
 flipCard(0);
 flipCard(2);
+
